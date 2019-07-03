d15d27dcdeca4df3e30f76b185b1f1e8572f1d75
Manoj Mishra <manojmishra.deb@gmail.com>
2019-07-03 17:04:03 +0530

    init

---
+++ apps/rumbl/web/models/user.ex
@@ -0,0 +1,40 @@
+defmodule Rumbl.User do
+  use Rumbl.Web, :model
+  # defstruct [:id, :name, :username, :password]
+
+  schema "users" do
+    field :name
+    field :username
+    field :password, :string, virtual: true
+    field :password_hash
+
+    has_many :videos, Rumbl.Video
+    has_many :annotations, Rumbl.Annotation
+    timestamps()
+  end
+
+  def changeset(user, params \\ %{}) do
+    user
+    |> cast(params, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+    |> unique_constraint(:username)
+  end
+
+  def registration_changeset(user, params) do 
+    user
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100) 
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do 
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} -> 
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
+end
